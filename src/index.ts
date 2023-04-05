6d90067230dfefab3fdf4507b1fbb5e609469639
wprestonneal <mrprestonneal@gmail.com>
2023-04-04 19:45:48 -0700

    finally works now

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
+import inquirer from 'inquirer'
+import * as fs from 'node:fs'
 
-console.log('hello from create-ts-app')
+const CHOICES = fs.readdirSync(`./templates`);
+
+const QUESTIONS = [
+    {
+      name: 'project-choice',
+      type: 'list',
+      message: 'What project template would you like to generate?',
+      choices: CHOICES
+    },
+    {
+      name: 'project-name',
+      type: 'input',
+      message: 'Project name:',
+      validate: function (input: string) {
+        if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+        else return 'Project name may only include letters, numbers, underscores and hashes.';
+      }
+    }
+  ];
+
+inquirer.prompt(QUESTIONS).then(answers => {
+    console.log(answers)
+})
