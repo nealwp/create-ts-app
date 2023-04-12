b12a1739ef978f0a1e42947aa76dc8d978154789
nealwp <mrprestonneal@gmail.com>
2023-04-11 22:15:39 -0700

    reading input works

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import inquirer from 'inquirer'
 import * as fs from 'node:fs'
+import * as path from 'node:path'
 
 const CHOICES = fs.readdirSync(`./templates`);
+const DIR = process.cwd()
 
 const QUESTIONS = [
     {
@@ -22,6 +24,20 @@ const QUESTIONS = [
     }
   ];
 
-inquirer.prompt(QUESTIONS).then(answers => {
+const prompt = inquirer.createPromptModule()
+ 
+prompt(QUESTIONS).then(answers => {
+    const projectChoice = answers['project-choice'];
+    const projectName = answers['project-name'];    
+    
+    const templatePath = path.join(DIR, 'templates', projectChoice);
+    const tartgetPath = path.join(DIR, projectName);    
+    const options = {
+        projectName,
+        templateName: projectChoice,
+        templatePath,
+        tartgetPath
+    }    
+    console.log(options);
     console.log(answers)
 })
