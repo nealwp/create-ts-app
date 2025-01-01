86f400d0ce249d27c50d4e34fa5a8260ec778c78
Preston Neal <mrprestonneal@gmail.com>
2024-12-31 16:02:26 -0800

    add basic service to http

--- templates/http-service/src/router.ts
+++ templates/http-service/src/router.ts
@@ -1,9 +1,41 @@
 import { Router } from "express";
+import service from "./service";
 
 const router = Router();
 
 router.get("/health", (_, res) => {
     return res.status(200).send();
+});
+
+router.get("/", (_, res) => {
+    try {
+        const examples = service.getExamples()
+        return res.status(200).json(examples);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send();
+    }
+});
+
+router.post("/", (req, res) => {
+    try {
+        const saved = service.saveExample(req.body)
+        return res.status(200).json(saved);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send();
+    }
+});
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleted = service.deleteExample(+id)
+        return res.status(200).json(deleted);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send();
+    }
 });
 
 export default router;
--- templates/http-service/tests/router.test.ts
+++ templates/http-service/tests/router.test.ts
@@ -6,11 +6,29 @@ import router from "../src/router";
 const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use("/", router);
+server.use("/api/example", router);
 const request = supertest(server);
 
-describe("GET /health", () => {
+describe("GET /api/example/health", () => {
     it("should return 200 status", async () => {
-        await request.get(`/health`).expect(200);
+        await request.get(`/api/example/health`).expect(200);
+    });
+});
+
+describe("GET /api/example", () => {
+    it("should return 200 status", async () => {
+        await request.get(`/api/example`).expect(200);
+    });
+});
+
+describe("POST /api/example", () => {
+    it("should return 200 status", async () => {
+        await request.post(`/api/example`).expect(200);
+    });
+});
+
+describe("DELETE /api/example/:id", () => {
+    it("should return 200 status", async () => {
+        await request.delete(`/api/example/1234`).expect(200);
     });
 });
